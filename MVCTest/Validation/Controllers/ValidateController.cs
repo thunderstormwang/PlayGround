02091b4adf1ebb3b35b2b731160b8ffa40bae73b
thunderstormwang <thunderstormwang@gmail.com>
2018-09-18 08:02:58 +0800

    Add Some Validation Code

---
+++ MVCTest/Validation/Controllers/ValidateController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class ValidateController : Controller
+    {
+        // GET: Validate
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(RequestBase input)
+        {
+            if (!ModelState.IsValid)
+            {
+                var messages = ModelState.Values
+                    .SelectMany(x => x.Errors)
+                    .Select(x => x.ErrorMessage);
+
+                return View(input);
+            }
+
+            return RedirectToAction("Result");
+        }
+
+        public ActionResult Result()
+        {
+            return View();
+        }
+    }
+}
