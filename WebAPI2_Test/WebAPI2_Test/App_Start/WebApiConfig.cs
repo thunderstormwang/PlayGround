2961106cfdc692a6f78456e831d79aa3c6258120
thunderstormwang <thunderstormwang@gmail.com>
2018-10-13 15:28:54 +0800

    加入 evernote 筆記內容

--- WebAPI2_Test/WebAPI2_Test/App_Start/WebApiConfig.cs
+++ WebAPI2_Test/WebAPI2_Test/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Http;
 
 namespace WebAPI2_Test
@@ -16,9 +14,16 @@ namespace WebAPI2_Test
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // 移除 Xml 格式
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            // 回傳的 Json 縮排換行
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
         }
     }
 }
---
+++ WebAPI2_Test/WebAPI2_Test/Controllers/TestController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using WebAPI2_Test.Models;
+
+namespace WebAPI2_Test.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet, HttpPost]
+        public HttpResponseMessage ReturnDictByXml()
+        {
+            var data = new Dictionary<string, string>()
+            {
+                { "Name", "Boyu" },
+                { "Company", "Gamble" },
+            };
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new ObjectContent<Dictionary<string, string>>(data, new XmlMediaTypeFormatter());
+
+            return resp;
+        }
+
+        [HttpGet, HttpPost]
+        public HttpResponseMessage ReturnObjectByXml()
+        {
+            Output output = new Output
+            {
+                Name = "HiHi",
+                TransactionNumber = "1234567"
+            };
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new ObjectContent<Output>(output, new XmlMediaTypeFormatter());
+
+            return resp;
+        }
+
+        [HttpGet, HttpPost]
+        public HttpResponseMessage ReturnDictByJson()
+        {
+            var data = new Dictionary<string, string>()
+            {
+                { "Name", "Boyu" },
+                { "Company", "Gamble" },
+            };
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new ObjectContent<Dictionary<string, string>>(data, new JsonMediaTypeFormatter());
+
+            return resp;
+        }
+
+        [HttpGet, HttpPost]
+        public HttpResponseMessage ReturnObjectByJson()
+        {
+            Output output = new Output
+            {
+                Name = "HiHi",
+                TransactionNumber = "1234567"
+            };
+
+            var resp = new HttpResponseMessage(HttpStatusCode.OK);
+            resp.Content = new ObjectContent<Output>(output, new JsonMediaTypeFormatter());
+
+            return resp;
+        }
+
+        [HttpGet, HttpPost]
+        public Dictionary<string, string> ReturnDict()
+        {
+            var data = new Dictionary<string, string>()
+            {
+                { "Name", "Boyu" },
+                { "Company", "Gamble" },
+            };
+
+            return data;
+        }
+
+        [HttpGet, HttpPost]
+        public Output ReturnObject()
+        {
+            Output output = new Output
+            {
+                Name = "HiHi",
+                TransactionNumber = "1234567"
+            };
+
+            return output;
+        }
+    }
+}
