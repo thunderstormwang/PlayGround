2134c08a7e8a8225aa155f11fac1d0ea14da12c2
thunderstormwang <thunderstormwang@gmail.com>
2018-12-17 22:11:41 +0800

    加入一些範例

--- WebAPI2_Test/WebAPI2_Test/Controllers/ModelBindingController.cs
+++ WebAPI2_Test/WebAPI2_Test/Controllers/ModelBindingController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using WebAPI2_Test.Models;
+using WebApiContrib.ModelBinders;
 
 namespace WebAPI2_Test.Controllers
 {
+    [MvcStyleBinding]
     public class ModelBindingController : ApiController
     {
         [HttpGet]
@@ -29,7 +26,31 @@ namespace WebAPI2_Test.Controllers
         [HttpGet, HttpPost]
         public string Demo3(int id, [FromBody]string companyName)
         {
-            return String.Format("ID: {0}, Company: {1}", id, companyName);
+            return string.Format("ID: {0}, Company: {1}", id, companyName);
         }
+
+        [HttpGet]
+        public string Demo4([FromUri]BaseRequest request)
+        {
+            return string.Format("Name: {0}, TransactionNumber: {1}", request.Name, request.TransactionNumber);
+        }
+
+        [HttpPost]
+        public string Demo5(BaseRequest request)
+        {
+            return string.Format("Name: {0}, TransactionNumber: {1}", request.Name, request.TransactionNumber);
+        }
+
+        [HttpGet, HttpPost]
+        public string Demo6(int id, BaseRequest request)
+        {
+            return string.Format("Name: {0}, TransactionNumber: {1}", request.Name, request.TransactionNumber);
+        }
+
+        public string Demo7(int id, BaseRequest request)
+        {
+            return string.Format("Name: {0}, TransactionNumber: {1}", request.Name, request.TransactionNumber);
+        }
+
     }
 }
--- WebAPI2_Test/WebAPI2_Test/Models/BaseRequest.cs
+++ WebAPI2_Test/WebAPI2_Test/Models/BaseRequest.cs
@@ -6,4 +6,20 @@
 
         public string TransactionNumber { get; set; }
     }
+
+    public class FirstRequest
+    {
+        public string A { get; set; }
+
+        public string B { get; set; }
+    }
+
+    public class SecondRequest
+    {
+        public string C { get; set; }
+
+        public string D { get; set; }
+    }
+
+
 }
