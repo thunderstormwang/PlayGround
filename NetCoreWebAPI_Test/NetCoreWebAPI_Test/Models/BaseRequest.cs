831666740f08eaa2a7d36e8954c1b32f0261edc1
thunderstormwang <thunderstormwang@gmail.com>
2019-04-07 11:20:24 +0800

    Add NLog

---
+++ NetCoreWebAPI_Test/NetCoreWebAPI_Test/Models/BaseRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreWebAPI_Test.Models
+{
+    public class BaseRequest
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ NetCoreWebAPI_Test/NetCoreWebAPI_Test/Models/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCoreWebAPI_Test.Models
+{
+    public class BaseResponse<T>
+    {
+        public bool Status { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public T Data { get; set; }
+    }
+}
