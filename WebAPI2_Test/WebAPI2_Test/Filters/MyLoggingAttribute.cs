74a7f5cff9115430bb76b7cfb4eefa8138198f73
thunderstormwang <thunderstormwang@gmail.com>
2019-05-15 20:56:38 +0800

    modify: log

--- WebAPI2_Test/WebAPI2_Test/Filters/MyLoggingAttribute.cs
+++ WebAPI2_Test/WebAPI2_Test/Filters/MyLoggingAttribute.cs
@@ -1,7 +1,7 @@
 ﻿using Newtonsoft.Json;
 using NLog;
-using System;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
@@ -11,41 +11,44 @@ namespace WebAPI2_Test.Filters
     {
         private static Logger _Logger = LogManager.GetCurrentClassLogger();
 
-        public override void OnActionExecuting(HttpActionContext actionContext)
-        {
-            // todo: remove  "\", "\n", "\t"
-
-            var logObj = new
-            {
-                RequestUri = actionContext.ControllerContext.Request.RequestUri.AbsolutePath,
-                RouteValue = JsonConvert.SerializeObject(actionContext.RequestContext.RouteData.Values),
-                QueryString = actionContext.ControllerContext.Request.RequestUri.Query,
-                RequestBody = GetRawRequest(actionContext),
-                ModelBindingRequest = actionContext.ActionArguments
-            };
-
-            _Logger.Info($"Begin {JsonConvert.SerializeObject(logObj)}");
-        }
-
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             var logObj = new
             {
                 RequestUri = actionExecutedContext.Request.RequestUri.AbsoluteUri,
-                RouteValue = JsonConvert.SerializeObject(actionExecutedContext.ActionContext.RequestContext.RouteData.Values),
+                RouteValue = actionExecutedContext.ActionContext.RequestContext.RouteData.Values,
                 QueryString = actionExecutedContext.ActionContext.Request.RequestUri.Query,
                 RequestBody = GetRawRequest(actionExecutedContext.ActionContext),
                 ModelBindingRequest = actionExecutedContext.ActionContext.ActionArguments,
                 ResponseBody = actionExecutedContext.Response != null ? actionExecutedContext.Response.Content.ReadAsStringAsync().Result : string.Empty
             };
 
-            _Logger.Info($"End {JsonConvert.SerializeObject(logObj)}");
+            _Logger.Info($"End {TrimChar(JsonConvert.SerializeObject(logObj))}");
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var logObj = new
+            {
+                RequestUri = actionContext.ControllerContext.Request.RequestUri.AbsolutePath,
+                RouteValue = actionContext.RequestContext.RouteData.Values,
+                QueryString = actionContext.ControllerContext.Request.RequestUri.Query,
+                RequestBody = GetRawRequest(actionContext),
+                ModelBindingRequest = actionContext.ActionArguments
+            };
+
+            _Logger.Info($"Begin {TrimChar(JsonConvert.SerializeObject(logObj))}");
         }
 
         private string GetRawRequest(HttpActionContext actionContext)
         {
             actionContext.Request.Content.ReadAsStreamAsync().Result.Seek(0, SeekOrigin.Begin);
             return actionContext.Request.Content.ReadAsStringAsync().Result;
+        }
+
+        private string TrimChar(string value)
+        {
+            return Regex.Replace(value, @"\t|\n", string.Empty);
         }
     }
 }
