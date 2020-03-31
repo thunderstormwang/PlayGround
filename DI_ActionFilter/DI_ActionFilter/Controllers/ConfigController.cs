9210086de9ce7304900ed74f0df4d522af98b7f2
RoodyWang <roodywang@boyu66.net>
2020-03-31 18:09:20 +0800

    add config example

---
+++ DI_ActionFilter/DI_ActionFilter/Controllers/ConfigController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DI_ActionFilter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigController : Controller
+    {
+        private IConfiguration Configuration { get; set; }
+
+        public ConfigController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("Test")]
+        public string Test()
+        {
+            Debug.WriteLine($"ID: {Configuration.GetValue<string>("Member:ID")}");
+            Debug.WriteLine($"Password: {Configuration.GetValue<string>("Member:Password")}");
+            Debug.WriteLine($"Url: {Configuration.GetValue<string>("Member:Url")}");
+            Debug.WriteLine($"Salary: {Configuration.GetValue<string>("Member:Salary")}");
+
+            return "Hello World";
+        }
+    }
+}
---
+++ DI_ActionFilter/DI_ActionFilter/Models/Member.cs
@@ -0,0 +1,10 @@
+﻿namespace DI_ActionFilter.Models
+{
+    public class Member
+    {
+        public string ID { get; set; }
+        public string Passwrod { get; set; }
+        public string Url { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
