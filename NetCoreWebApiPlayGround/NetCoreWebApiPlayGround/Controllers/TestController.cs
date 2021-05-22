46dd9493f97d3d9c909589737f98e9c9df67dc1f
ThunderstormWang <thunderstormwang@gmail.com>
2021-05-22 10:49:42 +0800

    Add ApiRunTimeAttribute

---
+++ NetCoreWebApiPlayGround/NetCoreWebApiPlayGround/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreWebApiPlayGround.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet("Echo")]
+        public string Echo()
+        {
+            Thread.Sleep(500);
+            return "Hello World";
+        }
+    }
+}
--- NetCoreWebApiPlayGround/NetCoreWebApiPlayGround/Startup.cs
+++ NetCoreWebApiPlayGround/NetCoreWebApiPlayGround/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NetCoreWebApiPlayGround.ActionFilters;
 
 namespace NetCoreWebApiPlayGround
 {
@@ -25,7 +26,11 @@ namespace NetCoreWebApiPlayGround
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                // 全域的註冊方式
+                options.Filters.Add(new ApiRunTimeAttribute());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
